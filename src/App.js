d4694200b3be3a7c307c9e5c91224f8e525e69eb
Zhengmao Zhang <zhengmao@pdx.edu>
2022-03-13 06:05:03 -0700

    add account feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
-import { Navbar, Footer, About} from './components';
+import { Navbar, Footer, About, GameAccounts} from './components';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <About />
-      <Footer/>
-    </div>
-    // <Router>
-    //     <Navbar/>
-    //     <Routes>
-    //     <Route
-    //         exact
-    //         index
-    //         // path="/"
-    //         element={<About />}
-    //       />
-    //     </Routes>
-    //     <Footer/>
-    // </Router>
+    // <div>
+    //   <Navbar/>
+    //   <About />
+    //   <Footer/>
+    // </div>
+    <Router>
+        <Navbar/>
+        <Routes>
+        <Route exact index element={<About />}/>
+        <Route exact path="/accounts" element={<GameAccounts/>}/>
+        </Routes>
+        {/* <Footer/> */}
+    </Router>
   );
 }
 
